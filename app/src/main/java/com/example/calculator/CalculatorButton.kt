322798c18503840c55da7572aa5bb9bf36430bab
waleed ahmed <waleedahmed1234555@gmail.com>
2024-07-17 11:01:38 +0500

    first commit

---
+++ app/src/main/java/com/example/calculator/CalculatorButton.kt
@@ -0,0 +1,39 @@
+package com.example.calculator
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun CalculatorButton(
+    symbol: String,
+    color: Color = Color.White,
+    modifier: Modifier,
+    onClick: () -> Unit,
+
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .clip(CircleShape)
+            .clickable { onClick() }
+            .then(modifier)
+    ) {
+        Text(
+            text = symbol,
+            fontSize = 34.sp,
+            color = color
+        )
+    }
+}
---
+++ app/src/main/java/com/example/calculator/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.calculator.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+
+val TextGreen = Color(0xFF5BB5A2)
+val BtnBGWhite = Color(0xFFFEFFFF)
+val BackgroundColor = Color(0xFF283637)
+val BtnBGDark = Color(0xFF212B2C)
